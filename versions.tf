705185d1659a6fe42a0f49182d08b6c62cbf5970
JO503688 <>
2026-03-10 14:00:13 -0600

    add terraform version file

--- versions.tf
+++ versions.tf
@@ -1,10 +1,12 @@
 terraform {
-  required_version = ">= 1.6"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.100"
+      version = "~>4.60"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
   }
 }
