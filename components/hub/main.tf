9494af4aa5293f1ab9a0b262021355e2522df64e
JO503688 <>
2026-03-12 11:35:20 -0600

    test terraform cloud azure authentication

--- components/hub/main.tf
+++ components/hub/main.tf
@@ -10,10 +10,18 @@ module "hub_network" {
   vnet_name           = var.vnet_name
   vnet_resource_group = var.vnet_resource_group
 
-  firewall_subnet_name    = var.firewall_subnet_name
+  firewall_subnet_name = var.firewall_subnet_name
+
   firewall_public_ip_name = var.firewall_public_ip_name
   firewall_public_ip_rg   = var.firewall_public_ip_rg
 
+  firewall_policy_name = var.firewall_policy_name
+  firewall_policy_rg   = var.firewall_policy_rg
+
   dns_inbound_subnet_id  = var.dns_inbound_subnet_id
   dns_outbound_subnet_id = var.dns_outbound_subnet_id
+}
+
+resource "random_pet" "test" {
+  length = 2
 }
